4505f3c38731d618ed6043e9549be072ad7f7508
makichiis <sevans.mailing@gmail.com>
2025-02-19 16:18:43 -0600

    Intermittent commit to merge with local branch

--- neo-explorer/public/index.js
+++ neo-explorer/public/index.js
@@ -1,8 +1,55 @@
 import './neo.js'
 
 // placeholder, replaced with dom interaction 
-const text = "\"Hello, world.\", 5 + 2";
+//const text = "\"Hello, world.\", 5 + 2";
 
-const source = libneo.source_alloc(text);
-const tokens = libneo.tokenize(source);
+//const source = libneo.source_alloc(text);
+//const tokens = libneo.tokenize(source);
 
+const handleTokenize = function (text) {
+  const start = performance.now();
+
+  const report = () => {
+    const end = performance.now();
+    console.log(`tokenization took ${end - start}ms`);
+  };
+
+  if (!text) {
+    output.textContent = "";
+    report();
+    return;
+  }
+
+  const source = libneo.source_alloc(text);
+  const tokens = libneo.tokenize(source);
+
+  try {
+    const tokenBuffer = new Uint16Array(
+      libneo.memory.buffer,
+      tokens.ptr,
+      tokens.len,
+    );
+    const byteBuffer = new Uint8Array(
+      tokenBuffer.buffer,
+      tokenBuffer.byteOffset,
+      tokenBuffer.byteLength,
+    );
+
+    let outputText = "";
+    for (let i = 0; i < byteBuffer.length; i += 2) {
+      const [tag, len] = [byteBuffer[i], byteBuffer[i + 1]];
+      outputText += `Token { ${libneo.token_name(tag)}, ${len} }\n`;
+
+      // TODO: Remove hardcoded eof check.
+      if (tag === 128) break;
+    }
+
+    console.log(outputText);
+  } finally {
+    libneo.free(tokens);
+    libneo.source_free(source);
+    report();
+  }
+};
+
+handleTokenize("This is awesome. 5 + 3 = 8");
