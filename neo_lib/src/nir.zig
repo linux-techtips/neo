a82441da8966679fb213df347d11520d6326f301
linuxtechtips <vavraacademy@gmail.com>
2025-04-09 16:53:31 -0500

    added basic and i mean basic nir evaluation

--- neo_lib/src/nir.zig
+++ neo_lib/src/nir.zig
@@ -17,6 +17,10 @@ pub const Inst = struct {
     pub const Index = u32;
 
     pub const Data = union {
+        decl: struct {
+            name: []const u8,
+            expr: Index,
+        },
         bin: struct {
             lhs: Index,
             rhs: Index,
@@ -25,7 +29,11 @@ pub const Inst = struct {
     };
 
     const Tag = enum(u8) {
+        decl,
         add,
+        sub,
+        mul,
+        div,
         int,
     };
 };
@@ -33,6 +41,7 @@ pub const Inst = struct {
 pub const Module = struct {
     code: std.MultiArrayList(Inst),
 
+    pub const Code = std.MultiArrayList(Inst).Slice;
     pub const new = Module{ .code = .empty };
 
     pub fn deinit(self: *Module, allocator: Allocator) void {
@@ -75,16 +84,20 @@ pub const Generator = struct {
     fn genToplevel(self: *Generator, ctx: Context) Error!Context {
         const token = self.tokens[ctx.tokens_idx];
 
-        if (!token.isOperator()) @panic("Expected an Operator at toplevel");
+        if (token.tag == .@":") return try self.genVarDecl(ctx);
+        if (token.isOperator()) return try self.genBinOp(ctx);
 
-        return try self.genBinOp(ctx);
+        std.debug.panic("Expected a toplevel expression but found: `{s}` instead.", .{@tagName(token.tag)});
     }
 
     fn genBinOp(self: *Generator, ctx: Context) Error!Context {
         const token = self.tokens[ctx.tokens_idx];
         const tag: Inst.Tag = switch (token.tag) {
             .@"+" => .add,
-            else => unreachable,
+            .@"-" => .sub,
+            .@"*" => .mul,
+            .@"/" => .div,
+            else => |tag| std.debug.panic("Invalid Binary Operator: {s}", .{@tagName(tag)}),
         };
 
         const inst = try self.module.addInst(self.allocator, .{
@@ -136,6 +149,55 @@ pub const Generator = struct {
         @panic("Expected to generate an operand or operator");
     }
 
+    fn genVarDecl(self: *Generator, ctx: Context) Error!Context {
+        // Tokens :, name, :, type, expr
+        // Source name, :, type, :, expr
+
+        const inst = try self.module.addInst(self.allocator, .{
+            .tag = .decl,
+            .token = self.tokens[ctx.tokens_idx],
+            .data = undefined,
+        });
+
+        const name = try self.parseIdent(.{
+            .source_idx = ctx.source_idx,
+            .tokens_idx = ctx.tokens_idx - 1, // Skip the ':' token.
+        });
+
+        const ty = try self.parseIdent(.{
+            .source_idx = self.skipWhitespace(name.source_idx + 1), // Skip the ':' source.
+            .tokens_idx = name.tokens_idx - 1, // Skip the ':' token.
+        });
+
+        const expr = try self.genBinOp(.{
+            .source_idx = self.skipWhitespace(ty.source_idx + 1), // Skip the ':' source.
+            .tokens_idx = ty.tokens_idx,
+        });
+
+        self.module.getDataRef(inst).decl = .{
+            .name = self.source[ctx.source_idx..self.tokens[name.tokens_idx + 1].len],
+            .expr = expr.inst,
+        };
+
+        return .{
+            .inst = inst,
+            .source_idx = expr.source_idx,
+            .tokens_idx = expr.tokens_idx,
+        };
+    }
+
+    fn parseIdent(self: *Generator, ctx: Context) Error!Context {
+        const token = self.tokens[ctx.tokens_idx];
+        const slice = self.source[ctx.source_idx..][0..token.len];
+
+        if (token.tag != .ident) std.debug.print("Expected an ident but found: `{s} - {s}` instead.", .{ @tagName(token.tag), slice });
+
+        return .{
+            .source_idx = self.skipWhitespace(ctx.source_idx + token.len),
+            .tokens_idx = ctx.tokens_idx - 1,
+        };
+    }
+
     fn skipWhitespace(self: *Generator, offset: u32) u32 {
         var i = offset;
         while (i < self.source.len and self.source[i] == ' ') : (i += 1) {}
@@ -143,10 +205,43 @@ pub const Generator = struct {
     }
 };
 
+const Evaluator = struct {
+    code: Module.Code,
+
+    pub fn evaluate(module: Module) u64 {
+        var evaluator = Evaluator{ .code = module.code.slice() };
+        return evaluator.evalExpr(0);
+    }
+
+    fn evalExpr(self: *Evaluator, index: Inst.Index) u64 {
+        const inst = self.code.get(index);
+        return switch (inst.tag) {
+            .add, .sub, .mul, .div => |tag| {
+                const lhs = self.evalExpr(inst.data.bin.lhs);
+                const rhs = self.evalExpr(inst.data.bin.rhs);
+
+                return Evaluator.evalBinOp(tag, lhs, rhs);
+            },
+            .decl => self.evalExpr(inst.data.decl.expr),
+            .int => inst.data.int,
+        };
+    }
+
+    fn evalBinOp(tag: Inst.Tag, lhs: u64, rhs: u64) u64 {
+        return switch (tag) {
+            .add => lhs +% rhs,
+            .sub => lhs -% rhs,
+            .mul => lhs *% rhs,
+            .div => lhs / rhs,
+            else => std.debug.panic("Not a binary operator: {s}", .{@tagName(tag)}),
+        };
+    }
+};
+
 test "generate" {
     const allocator = std.testing.allocator;
 
-    const source = try Source.fromText(allocator, "2 + 2 + 2");
+    const source = try Source.fromText(allocator, "x : u32 : 5 - 2 * 2");
     defer source.deinit(allocator);
 
     const tokens = try tokenizer.tokenize(allocator, source);
@@ -158,11 +253,9 @@ test "generate" {
     var module = try generate(allocator, source, tree);
     defer module.deinit(allocator);
 
-    const slice = module.code.slice();
-    for (0..module.code.len) |i| {
-        const inst = slice.get(i);
-        std.debug.print("{}\n", .{inst});
-    }
+    const result = evaluate(module);
+    std.debug.print("{}\n", .{result});
 }
 
 pub const generate = Generator.generate;
+pub const evaluate = Evaluator.evaluate;
